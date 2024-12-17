6f30edd07e78d7afcdf0c1290f0df64faeb97670
Jason Kirkham <jason.kirkham@gmail.com>
2024-12-17 09:34:23 +0000

    reverted and started again

--- gnome/configuration.nix
+++ gnome/configuration.nix
@@ -47,6 +47,31 @@
  	#allow unfree
 	nixpkgs.config.allowUnfree = true;
 
+  #enable firefox
+  programs.firefox.enable = true;
+  
+  # Your common configuration here
+  environment.systemPackages = with pkgs; [
+    git 
+    neovim
+		gedit	
+		alacritty
+		kitty
+		code-cursor
+    bat # A cat clone with syntax highlighting and Git integration
+		duf # Disk usage utility with a modern, intuitive interface
+    eza # Modern, maintained replacement for ls
+    git # Distributed version control system
+    gparted # Graphical disk partition editor
+		htop # Interactive process viewer and system monitor
+		nh # Nix helper
+		spotify # Music streaming service
+		tree # Recursive directory listing command
+    unrar # Utility for extracting RAR archives
+    unzip # Utility for extracting ZIP archives
+    vscodium # code writing
+  ];
+
 	#disable firewall
    networking.firewall.enable = false;
 
