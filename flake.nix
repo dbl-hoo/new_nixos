68747c6f0608a072fd2c4839b49c91d93a0491b1
Jason Kirkham <jason.kirkham@gmail.com>
2024-12-18 00:11:43 +0000

    revised to add hyprland

--- flake.nix
+++ flake.nix
@@ -1,35 +1,68 @@
 {
-
 	description = "Kirkham NixOS";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		
+		zen-browser = {
+			url = "github:MarceColl/zen-browser-flake";
+		};
+	};
 
 	outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
-    nixosConfigurations.gnome = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [ 
-      ./system/gnome/configuration.nix 
-      # ./modules/common.nix
-      home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.backupFileExtension = "HMBackup";
-          home-manager.useUserPackages = true;
-          home-manager.users.kirkham.imports = [
-            ./modules/home.nix
-          ];
-          home-manager.extraSpecialArgs = { inherit inputs; system = "x86_64-linux";};
-        }
-      ];
-    };
-  };
+		nixosConfigurations = {
+			# GNOME Configuration
+			gnome = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				specialArgs = { inherit inputs; };
+				modules = [ 
+					./system/gnome/configuration.nix 
+					
+					home-manager.nixosModules.home-manager {
+						home-manager = {
+							useGlobalPkgs = true;
+							useUserPackages = true;
+							backupFileExtension = "HMBackup";
+							users.kirkham.imports = [ ./modules/home.nix ];
+							extraSpecialArgs = { 
+								inherit inputs; 
+								system = "x86_64-linux";
+							};
+						};
+					}
+				];
+			};
+			
+			# Hyprland Configuration
+			hyprland = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				specialArgs = { inherit inputs; };
+				modules = [ 
+					./system/hyprland/configuration.nix 
+					
+					home-manager.nixosModules.home-manager {
+						home-manager = {
+							useGlobalPkgs = true;
+							useUserPackages = true;
+							backupFileExtension = "HMBackup";
+							users.kirkham.imports = [ 
+                ./modules/home.nix 
+                ./user/hyprland/hyprland.nix 
+              ];
+							extraSpecialArgs = { 
+								inherit inputs; 
+								system = "x86_64-linux";
+							};
+						};
+					}
+				];
+			};
+		};
+	};
 }
 
